796a2ec186b73bc71c18a73e67070718d2740c49
Henri Longle <33480010+longleh@users.noreply.github.com>
2023-09-25 18:38:34 +0200

    delete console.log

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,4 @@
 import 'phaser'
 import OpenWorld from '@scenes/OpenWorld'
 
-console.log({
-    width: screen.availWidth,
-    height: screen.availHeight,
-})
-
 const game = new Phaser.Game(OpenWorld.config)
